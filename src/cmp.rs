b2d0f4096c653430b78931a4895208aa1ad1926b
Lars Schumann <LarsSchumann@mail.de>
2026-06-12 17:30:09 +0200

    feat: add partialord and ord

---
+++ src/cmp.rs
@@ -0,0 +1,53 @@
+/// # Safety
+///
+/// TODO
+pub unsafe trait TrustedPartialEq<Rhs = Self>: PartialEq<Rhs> {}
+
+/// # Safety
+///
+/// TODO
+pub unsafe trait TrustedEq: Eq {}
+
+/// # Safety
+///
+/// TODO
+pub unsafe trait TrustedPartialOrd<Rhs = Self>: PartialOrd<Rhs> {}
+
+/// # Safety
+///
+/// TODO
+pub unsafe trait TrustedOrd: Ord {}
+
+// SAFETY: we trust std
+crate::macros::unsafe_impl_trait_for_types! {
+    unsafe impl TrustedPartialEq for [
+        u8, u16, u32, u64, u128, usize,
+        i8, i16, i32, i64, i128, isize,
+                 f32, f64,
+    ]
+}
+
+// SAFETY: we trust std
+crate::macros::unsafe_impl_trait_for_types! {
+    unsafe impl TrustedEq for [
+        u8, u16, u32, u64, u128, usize,
+        i8, i16, i32, i64, i128, isize,
+    ]
+}
+
+// SAFETY: we trust std
+crate::macros::unsafe_impl_trait_for_types! {
+    unsafe impl TrustedPartialOrd for [
+        u8, u16, u32, u64, u128, usize,
+        i8, i16, i32, i64, i128, isize,
+                 f32, f64,
+    ]
+}
+
+// SAFETY: we trust std
+crate::macros::unsafe_impl_trait_for_types! {
+    unsafe impl TrustedOrd for [
+        u8, u16, u32, u64, u128, usize,
+        i8, i16, i32, i64, i128, isize,
+    ]
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,4 @@
 #![no_std]
 
+pub mod cmp;
 mod macros;
-
-/// # Safety
-///
-/// TODO
-pub unsafe trait TrustedPartialEq<Rhs = Self>: PartialEq<Rhs> {}
-
-/// # Safety
-///
-/// TODO
-pub unsafe trait TrustedEq: Eq {}
-
-// SAFETY: we trust std
-crate::macros::unsafe_impl_trait_for_types! {
-    unsafe impl TrustedPartialEq for [
-        u8, u16, u32, u64, u128, usize,
-        i8, i16, i32, i64, i128, isize,
-                 f32, f64,
-    ]
-}
-
-// SAFETY: we trust std
-crate::macros::unsafe_impl_trait_for_types! {
-    unsafe impl TrustedEq for [
-        u8, u16, u32, u64, u128, usize,
-        i8, i16, i32, i64, i128, isize,
-    ]
-}
