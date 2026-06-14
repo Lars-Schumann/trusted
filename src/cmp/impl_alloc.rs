2e859d7acb2781ed00f4a65a50acb9f61e77d491
Lars Schumann <LarsSchumann@mail.de>
2026-06-14 18:21:02 +0200

    feat: support generics in impl macro

--- src/cmp/impl_alloc.rs
+++ src/cmp/impl_alloc.rs
@@ -1,1 +1,7 @@
-unsafe impl<T: crate::cmp::TrustedPartialEq> crate::cmp::TrustedPartialEq for ::alloc::vec::Vec<T> {}
+crate::macros::unsafe_impl_trait_for_types! {
+    unsafe impl crate::cmp::TrustedPartialEq [for] [
+        {T: crate::cmp::TrustedPartialEq} ::alloc::collections::vec_deque::VecDeque<T>,
+
+        {T: crate::cmp::TrustedPartialEq} ::alloc::vec::Vec<T>,
+    ]
+}
