73df3d681ca9f784d46061849037251abc8755bd
Lars Schumann <LarsSchumann@mail.de>
2026-06-16 21:30:32 +0200

    docs: add docs for TrustedEq

--- src/cmp/eq/mod.rs
+++ src/cmp/eq/mod.rs
@@ -7,7 +7,18 @@ mod impl_core;
 #[cfg(feature = "alloc")]
 mod impl_alloc;
 
+/// The primary difference to [`TrustedPartialEq`] is the additional requirement for reflexivity. A type
+/// that implements [`TrustedPartialEq`] guarantees that for all `a`, `b` and `c`:
+///
+/// - symmetric: `a == b` implies `b == a` and `a != b` implies `!(a == b)`
+/// - transitive: `a == b` and `b == c` implies `a == c`
+///
+/// `TrustedEq`, which builds on top of [`TrustedPartialEq`] also implies:
+///
+/// - reflexive: `a == a`
+///
 /// # Safety
 ///
-/// TODO
+/// Violating this property is a safety error.
+/// This means that `unsafe` code may rely on the correctness of these methods.
 pub unsafe trait TrustedEq: TrustedPartialEq<Self> + Eq {}
